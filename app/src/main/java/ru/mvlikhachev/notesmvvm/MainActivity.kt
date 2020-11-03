265c7cde227a63abece343933bd6c033ffba4a23
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-11-03 10:25:36 +0300

    Initialization objects in MainActivity

--- app/src/main/java/ru/mvlikhachev/notesmvvm/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/notesmvvm/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.mvlikhachev.notesmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import ru.mvlikhachev.notesmvvm.databinding.ActivityMainBinding
+import ru.mvlikhachev.notesmvvm.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        APP_ACTIVITY = this
+
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.navHostFragment)
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.title)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _binding = null
     }
 }
---
+++ app/src/main/java/ru/mvlikhachev/notesmvvm/utilits/constants.kt
@@ -0,0 +1,5 @@
+package ru.mvlikhachev.notesmvvm.utilits
+
+import ru.mvlikhachev.notesmvvm.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
