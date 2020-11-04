a685ca3951fd01455f73ce66aa62c78e82200c3f
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-11-04 10:34:12 +0300

    10. Create new note(bug with main thread)

---
+++ app/src/main/java/ru/mvlikhachev/notesmvvm/screens/add_new_note/AddNewNoteFragment.kt
@@ -0,0 +1,55 @@
+package ru.mvlikhachev.notesmvvm.screens.add_new_note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import ru.mvlikhachev.notesmvvm.R
+import ru.mvlikhachev.notesmvvm.databinding.FragmentAddNewNoteBinding
+import ru.mvlikhachev.notesmvvm.model.AppNote
+import ru.mvlikhachev.notesmvvm.utilits.APP_ACTIVITY
+import ru.mvlikhachev.notesmvvm.utilits.showToast
+
+
+class AddNewNoteFragment : Fragment() {
+
+
+    private var _binding: FragmentAddNewNoteBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mViewModel: AddNewNoteFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddNewNoteBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialization()
+    }
+
+    private fun initialization() {
+        mViewModel = ViewModelProvider(this).get(AddNewNoteFragmentViewModel::class.java)
+        mBinding.addNoteButton.setOnClickListener {
+            val name = mBinding.inputNameNoteEditText.text.toString()
+            val text = mBinding.inputTextNoteEditText.text.toString()
+            if (name.isEmpty()){
+                showToast(getString(R.string.toast_enter_name))
+            } else {
+                mViewModel.insert(AppNote(name = name,text = text)){
+                    APP_ACTIVITY.mNavController.navigate(R.id.action_addNewNoteFragment_to_mainFragment)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ru/mvlikhachev/notesmvvm/screens/add_new_note/AddNewNoteFragmentViewModel.kt
@@ -0,0 +1,19 @@
+package ru.mvlikhachev.notesmvvm.screens.add_new_note
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.mvlikhachev.notesmvvm.model.AppNote
+import ru.mvlikhachev.notesmvvm.utilits.REPOSITORY
+
+class AddNewNoteFragmentViewModel (application: Application) : AndroidViewModel(application) {
+
+    fun insert(note: AppNote, onSuccess: () -> Unit) =
+        viewModelScope.launch(Dispatchers.Main) {
+            REPOSITORY.insert(note){
+                onSuccess()
+            }
+        }
+}
